ea7047a0959e78149c2a712697474945f22837a5
jasonfletcher <jasonfletchersmail@gmail.com>
2020-06-30 14:07:33 -0600

    Added support for storing files in db

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_heroku import Heroku
 import io
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://sxdssutyixkwtn:a66c7e3ec173152ad7933fe9ebf11a86a7b5ef2e1662f0eb759c0af44210b01d@ec2-3-216-129-140.compute-1.amazonaws.com:5432/deoglt75m7qa5"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -14,6 +14,53 @@ ma = Marshmallow(app)
 heroku = Heroku(app)
 CORS(app)
 
+class File(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(), nullable=False)
+   file_type = db.Column(db.String(), nullable=False)
+   data = db.Column(db.LargeBinary, nullable=False)
+
+   def __init__(self, name, file_type, data):
+      self.name = name
+      self.file_type = file_type
+      self.data = data
+
+class FileSchema(ma.Schema):
+   class Meta:
+      fields = ("id", "name", "file_type")
+
+file_schema = FileSchema()
+files_schema = FileSchema(many=True)
+
+@app.route("/file/add", methods=["POST"])
+def add_file():
+   name = request.form.get('name')
+   file_type = request.form.get('type')
+   data = request.files.get('data')
+
+   new_file = File(name, file_type, data.read())
+   db.session.add(new_file)
+   db.session.commit()
+
+   return jsonify("File added successfully")
+
+@app.route("/file/get/data", methods=["GET"])
+def get_file_data():
+   file_data = db.session.query(File).all()
+   return jsonify(files_schema.dump(file_data))
+
+@app.route("/file/get/<id>", methods=["GET"])
+def get_file(id):
+   file_data = db.session.query(File).filter(File.id == id).first()
+   return send_file(
+      io.BytesIO(file_data.data), 
+      attachment_filename=file_data.name, 
+      mimetype=file_data.file_type
+   )
+
+# python repl
+# >>> from app import db
+# >>> db.create_all()
 
 if __name__ == "__main__":
    app.run(debug=True)
